8f98abe2ed69bcb0aec7796374b5563142c34a09
aitor <aitorman@outlook.com>
2023-03-07 18:39:56 +0100

    Implementado limpieza completa

--- prototipo.py
+++ prototipo.py
@@ -26,11 +26,13 @@ def search():
         query = f'{palabra_string.get()} since:{fecha_inicio_string.get()} until:{fecha_fin_string.get()}'
         ciudad = ciudad_string.get()
         usuario = usuario_string.get()
+        idioma = idioma_string.get()
         max_tweets = int(limite_int.get()) if limite_int.get() else 100
 
         # Preparar la búsqueda con parámetros adicionales si hubiera
         query += f' {"near:"+ciudad}' if ciudad else ''
         query += f' {"from:"+usuario}' if usuario else ''
+        query += f' {"lang:"+idioma.lower()}' if idioma else ''
 
         # Realizar la búsqueda
         tweets = []
@@ -93,12 +95,19 @@ def cleanData():
             # Limpiar los tweets
             # Eliminar los caracteres no occidentales
             columna_contenido = [re.sub(r'[^\x00-\x7F]+', '', texto) for texto in columna_contenido]
-            # Eliminar los signos de puntuación
+            # eliminar referencias html
+            columna_contenido = [re.sub(r'http\S+', '', texto) for texto in columna_contenido]
+            # eliminar referencias a usuarios
+            columna_contenido = [re.sub(r'@(\w+)', '', texto) for texto in columna_contenido]
+            # eliminar el "#" de los hashtags
+            columna_contenido = [re.sub(r'#', '', texto) for texto in columna_contenido]            
+            # Eliminar los signos de puntuación  ## ESTE REGEX SE HABRÍA DE REVISAR EN CASO DE CIERTOS IDIOMAS, ACENTOS ##
             columna_contenido = [re.sub(r'[^\w\s]', '', texto) for texto in columna_contenido]
             # Eliminar los números
             columna_contenido = [re.sub(r'\d+', '', texto) for texto in columna_contenido]
             # Eliminar los espacios en blanco
             columna_contenido = [re.sub(r'\s+', ' ', texto) for texto in columna_contenido]
+           
                 
             # tokeniza el texto en la columna utilizando la función 'word_tokenize' de la librería nltk
             tokens = [word_tokenize(texto) for texto in columna_contenido]
@@ -112,13 +121,9 @@ def cleanData():
             tokens = [[lemmatizer.lemmatize(palabra) for palabra in texto] for texto in tokens]
             # unir los tokens en una cadena de texto separados por espacios
             texto_limpio = [' '.join(texto) for texto in tokens]
-            # eliminar referencias html
-            texto_limpio = [re.sub(r'http\S+', '', texto) for texto in texto_limpio]
-            # eliminar referencias a usuarios
-            texto_limpio = [re.sub(r'@(\w+)', '', texto) for texto in texto_limpio]
-            # eliminar el "#" de los hashtags
-            texto_limpio = [re.sub(r'#', '', texto) for texto in texto_limpio]
-           
+            
+            
+        
             # añadir la columna con los tweets limpios al dataframe
             df['Contenido'] = texto_limpio
 
@@ -141,18 +146,21 @@ def cleanData():
 # Incio del widget
 
 widget = tk.Tk()
-widget.geometry('1000x370')
+widget.geometry('1000x400')
 widget.title(" TFG Aitor - Twitter")
 # Busqueda por palabra
-label_palabra = tk.Label(widget,text="Palabra a buscar: ").grid(column=0, row=0, sticky=tk.W)
+label_palabra = tk.Label(widget,text="Palabra o hastag a buscar: ").grid(column=0, row=0, sticky=tk.W)
 # Busqueda por fecha
-label_fecha_inicio = tk.Label(widget, text="Desde Fecha: (yyyy-mm-dd) ").grid(column=0, row=1, sticky=tk.W)
-label_fecha_fin = tk.Label(widget, text="Hasta fecha: (yyyy-mm-dd) ").grid(column=0, row=2, sticky=tk.W)
+label_fecha_inicio = tk.Label(widget, text="Desde: (yyyy-mm-dd) ").grid(column=0, row=1, sticky=tk.W)
+label_fecha_fin = tk.Label(widget, text="Hasta: (yyyy-mm-dd) ").grid(column=0, row=2, sticky=tk.W)
 # Busqueda por ciudad
-label_ciudad = tk.Label(widget, text="En Ciudad: ").grid(column=0, row=3, sticky=tk.W)
-label_usuario = tk.Label(widget, text="De Usuario: ").grid(column=0, row=4, sticky=tk.W)
+label_ciudad = tk.Label(widget, text="Twitteado desde (ciudad: ").grid(column=0, row=3, sticky=tk.W)
+# Busqueda por usuario
+label_usuario = tk.Label(widget, text="Twitteado por (usuario) : ").grid(column=0, row=4, sticky=tk.W)
+# Idioma de los tweets
+label_idioma = tk.Label(widget, text="Idioma de tweet (ISO --> en, es..) : ").grid(column=0, row=5, sticky=tk.W)
 # Limite de tweets
-label_limite = tk.Label(widget, text="Número Máximo Tweets: \n (por defecto 100)").grid(column=0, row=5, sticky=tk.W)
+label_limite = tk.Label(widget, text="Número Máximo Tweets: \n (por defecto 100)").grid(column=0, row=6, sticky=tk.W)
 
 # Variables
 palabra_string = tk.StringVar()
@@ -160,6 +168,7 @@ fecha_inicio_string = tk.StringVar()
 fecha_fin_string = tk.StringVar()
 ciudad_string = tk.StringVar()
 usuario_string = tk.StringVar()
+idioma_string = tk.StringVar()
 limite_int = tk.StringVar()
 
 # Entradas de texto
@@ -168,13 +177,14 @@ entry_fecha_inicio = tk.Entry(widget, width=30, textvariable=fecha_inicio_string
 entry_fecha_fin = tk.Entry(widget, width=30, textvariable=fecha_fin_string).grid(column=1, row=2, padx=10)
 entry_ciudad = tk.Entry(widget, width=30, textvariable=ciudad_string).grid(column=1, row=3, padx=10)
 entry_usuario = tk.Entry(widget, width=30, textvariable=usuario_string).grid(column=1, row=4, padx=10)
-entry_limite = tk.Entry(widget, width=30, textvariable=limite_int).grid(column=1, row=5, padx=10)
+entry_idioma = tk.Entry(widget, width=30, textvariable=idioma_string).grid(column=1, row=5, padx=10)
+entry_limite = tk.Entry(widget, width=30, textvariable=limite_int).grid(column=1, row=6, padx=10)
 
 # Botones
-button_resultados = tk.Button(widget, text='Buscar Tweets', command=search).grid(column=1, row=6, pady=10, sticky=tk.W)
-button_t2excel = tk.Button(widget, text='Guardar Resultados en Excel', command=storesearch).grid(column=1, row=7, pady=10, sticky=tk.W)
-button_limpiar = tk.Button(widget, text='Limpia los datos', command=cleanData).grid(column=1, row=8, pady=10, sticky=tk.W)
-button_quit = tk.Button(widget, text="Salir", command=widget.quit).grid(column=1, row=10, pady=10, sticky=tk.W)
+button_resultados = tk.Button(widget, text='Buscar Tweets', command=search).grid(column=1, row=7, pady=10, sticky=tk.W)
+button_t2excel = tk.Button(widget, text='Guardar Resultados en Excel', command=storesearch).grid(column=1, row=8, pady=10, sticky=tk.W)
+button_limpiar = tk.Button(widget, text='Limpia los datos', command=cleanData).grid(column=1, row=9, pady=10, sticky=tk.W)
+button_quit = tk.Button(widget, text="Salir", command=widget.quit).grid(column=1, row=11, pady=10, sticky=tk.W)
 # Icono
 icono = tk.PhotoImage(file="icono.png")
 label_icono = tk.Label(image=icono).grid(column=0, row=6, rowspan=6, padx=8)
